124e25b9d8b798aa1846df7fc91d86a10a1fdbb7
Travis Strawbridge <strtk001@mymail.unisa.edu.au>
2024-03-02 18:16:24 +1030

    Add project files.

---
+++ ccp_prac3/ccp_prac3.cpp
@@ -0,0 +1,12 @@
+﻿// ccp_prac3.cpp : Defines the entry point for the application.
+//
+
+#include "ccp_prac3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
